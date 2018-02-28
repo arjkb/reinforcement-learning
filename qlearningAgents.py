5a77f124d3c650102e84ea6f1f6df5ae8194aac6
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-02-27 20:31:30 -0700

    use a map() to add q-values to q_list in computeValueFromQValues()

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -69,8 +69,7 @@ class QLearningAgent(ReinforcementAgent):
         "*** YOUR CODE HERE ***"
         # util.raiseNotDefined()
         q_list = list()
-        for action in self.getLegalActions(state):
-            q_list.append(self.getQValue(state, action))
+        map(lambda ac: q_list.append(self.getQValue(state, ac)), self.getLegalActions(state))
         return max(q_list) if (len(q_list)) > 0 else 0
 
     def computeActionFromQValues(self, state):
