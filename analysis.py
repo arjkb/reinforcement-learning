57cce8bd5575bb1d4112b89de6a23daaae3864c0
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-02-28 09:42:39 -0700

    tune params for question 3c. 3c passes.

--- analysis.py
+++ analysis.py
@@ -39,10 +39,9 @@ def question3b():
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
-    return 'NOT POSSIBLE'
+    answerDiscount = 0.1
+    answerNoise = 0
+    answerLivingReward = 0.9
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
